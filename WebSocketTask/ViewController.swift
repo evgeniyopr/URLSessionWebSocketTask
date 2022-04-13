5af536880c139f1526c9459a4fc98e245883df20
Evgeniy Opryshko <>
2022-04-13 13:59:29 +0300

    init project

---
+++ WebSocketTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WebSocketTask
+//
+//  Created by Evgeniy Opryshko on 13.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
